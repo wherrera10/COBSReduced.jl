0e5d7df42608027f22a38a4b00fa0305bf26a85d
Bill <wherrera10@users.noreply.github.com>
2023-09-26 21:27:41 -1000

    Update COBSPackets.jl

--- src/COBSPackets.jl
+++ src/COBSPackets.jl
@@ -1,11 +1,12 @@
-module COBSPackets
+module COBSR
 
-export COBSencode, COBSdecode
+export COBSencode, COBSdecode, COBSRencode, COBSRdecode
 
 """
     COBSencode(data)
 
-Return result of encoding `inputdata` into COBS packet format.
+    Return result of encoding `inputdata` into COBS packet format.
+    Marker defaults to zero but may be any byte from 0 to 254.
 """
 function COBSencode(inputdata, marker = 0x00)
     output = [0xff]
@@ -35,9 +36,52 @@ function COBSencode(inputdata, marker = 0x00)
 end
 
 """
-    COBSdecode(buffer)
+    COBSRencode(data, marker = 0x00)
+
+    Return result of encoding `inputdata` into COBS/R packet format.
+    Marker defaults to zero but may be any byte from 0 to 254.
+    See also: pythonhosted.org/cobs/cobsr-intro.html
+"""
+function COBSRencode(inputdata, marker = 0x00)
+    output = [0xff]
+    codeindex, lastindex, code = 1, 1, 1
+    addlastcode = true
+    for byte in inputdata
+        if byte != marker
+            push!(output, byte)
+            code += 1
+        end
+        addlastcode = true
+        if byte == marker || code == 255
+            code == 255 && (addlastcode = false)
+            output[codeindex] = code
+            code = 1
+            push!(output, 0xff)
+            codeindex = length(output)
+            byte == marker && (lastindex = codeindex)
+        end
+    end
+    if addlastcode
+        output[codeindex] = code
+        push!(output, marker)
+    else
+        output[codeindex] = marker
+    end
+    # Reduce size output of by 1 char if can
+    if lastindex > 1 && output[end-1] + lastindex > length(output)
+        output[lastindex] = output[end-1]
+        output[end-1] = marker
+        pop!(output)
+    end
+    return output
+end
+
+
+"""
+    COBSdecode(buffer, marker = 0x00)
 
     Return result of decoding `buffer` from COBS encoded format.
+    The marker must be the same as was used for encode (defaults to zero).
 """
 function COBSdecode(buffer::AbstractVector, marker = 0x00)
     decoded = UInt8[]
@@ -45,13 +89,40 @@ function COBSdecode(buffer::AbstractVector, marker = 0x00)
     while bdx < len
         code = buffer[bdx]
         bdx += 1
-        for i in 1:code-1
+        for _ = 1:code-1
             push!(decoded, buffer[bdx])
             bdx += 1
-            bdx >= len && break
+            bdx > len && break
         end
         code < 0xff && bdx < len && push!(decoded, marker)
     end
+    return decoded
+end
+
+"""
+    COBSRdecode(buffer, marker = 0x00)
+
+    Return result of decoding `buffer` from COBS/R encoded format.
+    The marker must be the same as was used for encode (defaults to zero).
+    See also: pythonhosted.org/cobs/cobsr-intro.html
+"""
+function COBSRdecode(buffer::AbstractVector, marker = 0x00)
+    decoded = UInt8[]
+    bdx, len = 1, length(buffer)
+    lpos, lchar = 1, marker
+    while bdx < len
+        code = buffer[bdx]
+        lpos, lchar = bdx, code
+        bdx += 1
+        for _ = 1:code-1
+            push!(decoded, buffer[bdx])
+            bdx += 1
+            bdx > len && break
+        end
+        code < 0xff && bdx < len && push!(decoded, marker)
+    end
+    # Restore from reduced format if present
+    lchar != marker && lchar + lpos > len && (decoded[end] = lchar)
     return decoded
 end
 
